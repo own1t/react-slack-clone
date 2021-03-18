6c05773cc933354dd80eafa2fc406c79ecc00426
Ryan Kim <iawal86@gmail.com>
2021-03-17 18:02:39 -0700

    Slack Clone built with ReactJS, Redux, and Firebase

---
+++ README.md
@@ -0,0 +1,13 @@
+![alt text](https://github.com/own1t/react-slack-clone/blob/main/preview/slack-clone.png)
+
+## Slack Clone
+
+- Leveraged speed of loading application with using Styled Component
+
+- Integrated Redux for state management of chat rooms
+
+- Implemented Google authentication and database for storing messages with Firebase
+
+- Deployed the application with using Firebase Hosting
+
+You can check the application from https://slack-clone-3b2a8.web.app/
---
+++ src/pages/App.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import styled from "styled-components";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+
+import Spinner from "react-spinkit";
+
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+import Login from "./Login";
+
+function App() {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
+
+  return (
+    <div className="app">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route path="/" exact>
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding-bottom: 100px;
+
+  > img {
+    height: 100px;
+    margin-bottom: 50px;
+    padding: 20px;
+  }
+`;
